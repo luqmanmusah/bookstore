bd910188f8333afd9d00fdc5e3bdb9c96f3e8127
Luqman Musah <musahluqman@gmail.com>
2021-11-03 17:12:25 +0000

    Comment out unnecessary files

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import book from './book';
+// import React from 'react';
+// import book from './book';
 
-export default books() {
-  const book = {
-    title: 'Democracy The God That Failed',
-    author: 'Hans-Hermann Hoppe',
-  };
+// export default books() {
+//   const book = {
+//     title: 'Democracy The God That Failed',
+//     author: 'Hans-Hermann Hoppe',
+//   };
 
-  const books = [book];
-  return (
-    <ul>
-      {
-        Array.isArray(books) ? books.map((book) => <book key={book.id} book={book} />) : 'No Books yet'
-      }
-    </ul>
-  );
-};
+//   const books = [book];
+//   return (
+//     <ul>
+//       {
+
+//       }
+//     </ul>
+//   );
+// };
