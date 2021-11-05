5c3b8750b59c7ceb0b617697632a4e44bd702af6
Luqman Musah <musahluqman@gmail.com>
2021-11-05 15:31:41 +0000

    Add fetch and remove Book API

--- src/API/Api.js
+++ src/API/Api.js
@@ -6,15 +6,37 @@ const uniId = 'zrsfFNvt20pe6hjnRwcn';
 const appURL = `${baseURL}${uniId}/books/`;
 
 const addBookAPI = async (book) => {
-    axios.post(appURL, book)
+  axios.post(appURL, book)
     .then((response) => response)
     .catch((error) => {
-        throw new Error(error);
+      throw new Error(error);
     });
 };
 
 const fetchBookAPI = async () => {
-    const APIData = [];
+  const APIData = [];
 
-    
-}
+  const response = await axios.get(appURL);
+  Object.entries(response.data).forEach((item) => {
+    const bookID = item[0];
+    const [{ title, author }] = item[1];
+
+    const objAPI = {
+      item_id: bookID,
+      title,
+      author,
+    };
+
+    APIData.push(objAPI);
+  });
+
+  return APIData;
+};
+
+const removeBookAPI = async (id) => {
+  axios.delete(`${appURL}${id}`, {
+    item_id: id,
+  });
+};
+
+export { addBookAPI, removeBookAPI, fetchBookAPI };
